929aa1bc03805954d0ab7e45dc37cff55299c3f2
jeong <jlim8612@gmail.com>
2015-05-24 22:50:37 -0700

    Serve static, change css

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var $ = require('cheerio');
 
 app.set('trust proxy', true);
 
+app.use(express.static('./'));
+
 app.all('*', function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
